45e929820c49a722d32e4cc8d5ca1f4012abe9e6
Andy Dang <andy.dang.it@gmail.com>
2020-03-09 23:07:27 -0500

    13. Building a (Very) Simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -35,15 +35,23 @@ const hturltp = require('url');
 /////////////////////////////////
 // Server
 
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+    const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const pathName = req.url;
 
-    if(pathName === '/' || pathName === '/overview') { 
+    if (pathName === '/' || pathName === '/overview') { 
         res.end('This is the OVIERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
-    } else {
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(data);
+        
+    }else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
