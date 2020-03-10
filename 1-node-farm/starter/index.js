2cadd8aafdf321941364437064be164521610b6f
Andy Dang <andy.dang.it@gmail.com>
2020-03-09 22:29:05 -0500

    12. Routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const hturltp = require('url');
 
 /////////////////////////////
 // Files
@@ -29,13 +30,28 @@ const http = require('http');
     //     });
     // });
     // console.log('Will read file!');
+
+
 /////////////////////////////////
 // Server
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    console.log(req.url);
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') { 
+        res.end('This is the OVIERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found! </h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to the requests on port 8000');
-})
+});
